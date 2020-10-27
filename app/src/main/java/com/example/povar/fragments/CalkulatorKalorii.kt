4b36f8a60c4f72559d7d3dca2c5657ce0956952e
NikitaPetrenko17488 <17488@gapps.ispu.ru>
2020-10-27 16:57:42 +0300

    Калькулятор каллорий 2/2

--- app/src/main/java/com/example/povar/fragments/CalkulatorKalorii.kt
+++ app/src/main/java/com/example/povar/fragments/CalkulatorKalorii.kt
@@ -1,6 +1,7 @@
 package com.example.povar.fragments
 
 import DataAdapterCalkulatorKalorii
+import android.graphics.Color
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
@@ -8,18 +9,27 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.annotation.IntegerRes
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.povar.R
 import com.example.povar.models.CalculatorKalorii
 import com.example.povar.ui.*
-import com.google.android.gms.actions.ItemListIntents
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.fragment_calkulator_kalorii.*
-import kotlinx.android.synthetic.main.fragment_kalorii.*
 import kotlinx.android.synthetic.main.list_calculator_kalorii.*
+import kotlinx.android.synthetic.main.list_calculator_kalorii.view.*
+import org.w3c.dom.Text
+import kotlin.reflect.typeOf
 
-var MassivCalkulator= mutableListOf<CalculatorKalorii>()
+private var MassivCalkulator= mutableListOf<CalculatorKalorii>()
+private var counterCalkulator=0
+private var Gramm=0
+private var KaloriiGramm=0
+private var Res:Float= 0F
+private var ResInt=0
+
+
 
 
 
@@ -31,9 +41,15 @@ class CalkulatorKalorii : Fragment() {
         hideUserNameAdnImage(activity!!)
         hideSearch(activity!!)
         hideAddButton(activity!!)
-        language()
+       // tema()
+        //language()
         //init()
         MassivCalkulator.removeAll { true }
+         counterCalkulator=0
+         Gramm=0
+         KaloriiGramm=0
+         Res= 0F
+         ResInt=0
 
     }
 
@@ -52,15 +68,14 @@ class CalkulatorKalorii : Fragment() {
         hideUserNameAdnImage(activity!!)
         hideSearch(activity!!)
         hideAddButton(activity!!)
+        tema()
         language()
         //init()
         createRecycler()
         addSlotCalkulatorKaloriiAbstract.setOnClickListener {
 
-            init()
-            createRecycler()
+            addSlotAndSumm()
         }
-
 
     }
 
@@ -86,6 +101,36 @@ class CalkulatorKalorii : Fragment() {
             mCalorii?.text = movie.KaloririiGramm
             mGramm?.text =movie.Gramm
 
+            /////////TEMA
+            if(STORAGE.Tema==true) {
+                itemView.TextNameIngridients.setTextColor(Color.parseColor("#b2b2b2"))
+                itemView.TextKaloriiGramm.setTextColor(Color.parseColor("#b2b2b2"))
+                itemView.TextGramm.setTextColor(Color.parseColor("#b2b2b2"))
+
+                itemView.EditNameIngridients.setTextColor(Color.parseColor("#b2b2b2"))
+                itemView.EditKaloriiGramm.setTextColor(Color.parseColor("#b2b2b2"))
+                itemView.EditGramm.setTextColor(Color.parseColor("#b2b2b2"))
+            }
+            else{
+                itemView.TextNameIngridients.setTextColor(Color.parseColor("#000000"))
+                itemView.TextKaloriiGramm.setTextColor(Color.parseColor("#000000"))
+                itemView.TextGramm.setTextColor(Color.parseColor("#000000"))
+
+                itemView.EditNameIngridients.setTextColor(Color.parseColor("#000000"))
+                itemView.EditKaloriiGramm.setTextColor(Color.parseColor("#000000"))
+                itemView.EditGramm.setTextColor(Color.parseColor("#000000"))
+            }
+            if(STORAGE.Language=="Rus") {
+                itemView.TextNameIngridients.setText("Название ингредиента: ")
+                itemView.TextKaloriiGramm.setText("Ккл/100гр: ")
+                itemView.TextGramm.setText("Грамм: ")
+            }
+            else{
+                itemView.TextNameIngridients.setText("Ingredient name: ")
+                itemView.TextKaloriiGramm.setText("Kcal/100gr: ")
+                itemView.TextGramm.setText("Gram: ")
+            }
+
         }
     }
 
@@ -102,20 +147,100 @@ class CalkulatorKalorii : Fragment() {
 
     private fun language() {
 
+        activity!!.toolbar.setTitle("Калькулятор калорий")
         if (STORAGE.Language == "Rus") {
-            activity!!.toolbar.setTitle("Калькулятор калорий")
+            TextGrammAbstract.setText("Грамм: ")
+            TextKaloriiGrammAbstract.setText("Ккл/100гр: ")
+            TextNameIngridientsAbstract.setText("Название ингредиента: ")
+            TextSumm.setText("Всего: ")
+
+            EditNameIngridientsAbstract.setHint("Название")
+            EditKaloriiGrammAbstract.setHint("000 Ккл")
+            EditGrammAbstract.setHint("000 грамм")
+
         } else {
             activity!!.toolbar.setTitle("Calorie calculator")
+            TextGrammAbstract.setText("Gram: ")
+            TextKaloriiGrammAbstract.setText("Kcal/100gr: ")
+            TextNameIngridientsAbstract.setText("Ingredient name: ")
+            TextSumm.setText("Total: ")
+
+            EditNameIngridientsAbstract.setHint("Name")
+            EditKaloriiGrammAbstract.setHint("000 Kcal")
+            EditGrammAbstract.setHint("000 gr")
         }
     }
 
-    private fun init() {
-        MassivCalkulator.add(
-            CalculatorKalorii(EditNameIngridientsAbstract.text.toString(),EditKaloriiGrammAbstract.text.toString(),EditGrammAbstract.text.toString()
-        ))
-        EditNameIngridientsAbstract.setText("")
-        EditKaloriiGrammAbstract.setText("")
-        EditGrammAbstract.setText("")
+    private fun addSlotAndSumm() {
+
+
+            if (EditNameIngridientsAbstract.text.toString().isNotEmpty() && EditKaloriiGrammAbstract.text.toString().isNotEmpty() && EditGrammAbstract.text.toString().isNotEmpty())
+            {
+                    MassivCalkulator.add(
+                        CalculatorKalorii(
+                            EditNameIngridientsAbstract.text.toString(),
+                            EditKaloriiGrammAbstract.text.toString(),
+                            EditGrammAbstract.text.toString()
+                        )
+                    )
+                    EditNameIngridientsAbstract.setText("")
+                    EditKaloriiGrammAbstract.setText("")
+                    EditGrammAbstract.setText("")
+
+                    Gramm = MassivCalkulator[counterCalkulator].Gramm.toInt()
+                    KaloriiGramm = MassivCalkulator[counterCalkulator].KaloririiGramm.toInt()
+                    Res += KaloriiGramm.toFloat() * (Gramm.toFloat() / 100)
+                    ResInt= (Res*100).toInt()
+                    Res= (ResInt.toFloat()/100)
+                    Summ.setText(Res.toString())
+
+                    createRecycler()
+                    counterCalkulator++
+                }
+                else{
+                    showToast("Заполните все поля")
+                }
+
+
+
+    }
+
+
+
+    private fun tema(){
+
+
+        EditNameIngridientsAbstract.setHintTextColor(Color.parseColor("#808080"))
+        EditKaloriiGrammAbstract.setHintTextColor(Color.parseColor("#808080"))
+        EditGrammAbstract.setHintTextColor(Color.parseColor("#808080"))
+        if(STORAGE.Tema==true) {
+            Constraint_layout_Calkulator.setBackgroundResource(R.drawable.background_fon_fragment_dark_them)
+            TextSumm.setTextColor(Color.parseColor("#b2b2b2"))
+             Summ.setTextColor(Color.parseColor("#b2b2b2"))
+
+            TextNameIngridientsAbstract.setTextColor(Color.parseColor("#b2b2b2"))
+            TextKaloriiGrammAbstract.setTextColor(Color.parseColor("#b2b2b2"))
+            TextGrammAbstract.setTextColor(Color.parseColor("#b2b2b2"))
+            EditNameIngridientsAbstract.setTextColor(Color.parseColor("#b2b2b2"))
+            EditKaloriiGrammAbstract.setTextColor(Color.parseColor("#b2b2b2"))
+            EditGrammAbstract.setTextColor(Color.parseColor("#b2b2b2"))
+
+
+        }
+        else{
+            Constraint_layout_Calkulator.setBackgroundResource(R.drawable.background_fon_na_fragment_lite)
+            TextSumm.setTextColor(Color.parseColor("#000000"))
+            Summ.setTextColor(Color.parseColor("#000000"))
+
+            TextNameIngridientsAbstract.setTextColor(Color.parseColor("#000000"))
+            TextKaloriiGrammAbstract.setTextColor(Color.parseColor("#000000"))
+            TextGrammAbstract.setTextColor(Color.parseColor("#000000"))
+            EditNameIngridientsAbstract.setTextColor(Color.parseColor("#000000"))
+            EditKaloriiGrammAbstract.setTextColor(Color.parseColor("#000000"))
+            EditGrammAbstract.setTextColor(Color.parseColor("#000000"))
+            EditNameIngridientsAbstract.setHintTextColor(Color.parseColor("#C0C0C0"))
+
+        }
 
     }
 }
